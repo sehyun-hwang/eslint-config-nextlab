149ba85239333df59201ea05b818e120ac37f498
Sehyun Hwang <hwanghyun3@gmail.com>
2023-07-01 00:14:34 +0900

    eslint test script, add typescript support

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,12 @@ FROM node:alpine
 
 WORKDIR /mnt
 COPY package.json pnpm-lock.yaml ./
-
 RUN corepack enable pnpm \
-    && pnpm i --force --frozen-lockfile
+    && pnpm i --force --frozen-lockfile \
+    && pnpm store prune
 
-COPY eslint* index.* ./
+COPY . .
+RUN pnpm test
 
 ENV ESLINT_USE_FLAT_CONFIG=true
-RUN pnpm eslint index.*
+CMD ["pnpm", "lint"]
--- eslint.config.js
+++ eslint.config.js
@@ -1,35 +1,82 @@
-import path from "path";
-import { fileURLToPath } from "url";
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import importPlugin from 'eslint-plugin-import';
 
-import lagacyConfig from './eslint.json' assert {type: 'json'};
+import { jsRules, tsRules } from './config/index.js';
 
+// eslint-disable-next-line no-underscore-dangle
 const __filename = fileURLToPath(import.meta.url);
+// eslint-disable-next-line no-underscore-dangle
 const __dirname = path.dirname(__filename);
-
-const { rules } = lagacyConfig;
-const localConfig = await import(process.cwd() + '/eslint.config.js')
-  .catch(error => {
-    console.log(error);
-    return [];
-  });
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
+const { default: localConfig } = await import(
+  __dirname === process.cwd() ? './config/eslint.config.mock.js' : (process.cwd() + '/eslint.config.js')
+)
+  .catch(console.log) || {};
 
+/**
+@see https://github.com/import-js/eslint-plugin-import/issues/2556#issuecomment-1419518561
+*/
 export default [
-
-  ...compat.extends("airbnb-base"),
-
   {
-    files: ["**/*.js"],
+    // All eslint-plugin-import config is here
     languageOptions: {
-      ecmaVersion: "latest"
+      parserOptions: {
+        // Eslint doesn't supply ecmaVersion in `parser.js` `context.parserOptions`
+        // This is required to avoid ecmaVersion < 2015 error or 'import' / 'export' error
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
     },
-    rules,
+    plugins: {
+      patchedImport: importPlugin,
+    },
+    settings: {
+      // This will do the trick
+      'import/parsers': {
+        espree: ['.js', '.cjs', '.mjs', '.jsx'],
+      },
+      'import/resolver': {
+        node: true,
+      },
+    },
   },
 
-  ...localConfig
+  ...compat.extends('airbnb-base'),
+
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+    },
+    rules: jsRules,
+  },
+  {
+    files: ['*config.js', '*config.ts'],
+    rules: {
+      'import/no-extraneous-dependencies': 'off',
+    },
+  },
+
+  {
+    files: ['*config.js', '*config.ts'],
+    rules: {
+      'import/no-extraneous-dependencies': 'off',
+    },
+  },
+
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+    },
+    rules: tsRules,
+  },
+
+  ...localConfig,
 ];
