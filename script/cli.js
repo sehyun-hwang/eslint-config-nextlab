4a6b9186fe6323649e9ec62c7a841c75fe1b1130
Sehyun Hwang <hwanghyun3@gmail.com>
2023-11-14 03:36:49 +0000

    Better cli

--- script/cli.js
+++ script/cli.js
@@ -1,24 +1,28 @@
 #!/usr/bin/env node
 import { fileURLToPath } from 'url';
+import { join } from 'path';
 
-export function getEslintPath() {
-  console.log(import.meta.url);
-
+export function resolveImportMeta(path) {
   if (!import.meta.resolve)
     throw new Error(`import.meta.resolve is not a function.
        Please use NODE_OPTIONS=--experimental-import-meta-resolve env`);
 
-  return fileURLToPath(import.meta.resolve('../config/eslint4vite.js'));
-}
-
-let eslintPath = null;
-try {
-  eslintPath = getEslintPath();
-} catch (error) {
-  console.log(error);
+  return fileURLToPath(import.meta.resolve(path));
 }
 
 export const vitePluginConfig = {
-  eslintPath,
+  get eslintPath() {
+    return resolveImportMeta('../config/eslint4vite.js');
+  },
   fix: process.stdin.isTTY,
 };
+
+if (process.argv[1].endsWith('nextlab-eslint') || process.argv[1] === fileURLToPath(import.meta.url)) {
+  const eslintPath = join(resolveImportMeta('eslint'), '../../bin/eslint.js');
+  const configPath = resolveImportMeta('../eslint.config.js');
+  process.argv.push('-c', configPath);
+
+  console.log({ configPath, eslintPath });
+  await import(eslintPath);
+} else
+  console.log(process.argv);
