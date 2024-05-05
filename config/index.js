fc27e353f5a5d3ef5c6f564af1d99a7769369a54
Sehyun Hwang <hwanghyun3@gmail.com>
2024-05-05 22:27:01 +0900

    import syntax change: import ~ assert -> import ~ with

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
-import jsConfig from '../.eslintrc.json' assert {type: 'json'};
-import tsConfig from './typescript.eslintrc.json' assert {type: 'json'};
+import jsConfig from '../.eslintrc.json' with {type: 'json'};
+import tsConfig from './typescript.eslintrc.json' with {type: 'json'};
 
 export const jsRules = jsConfig.rules;
 export const tsRules = tsConfig.rules;
